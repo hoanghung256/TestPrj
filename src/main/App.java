cee6083088a3e22d3938c2d772bbf070044e5362
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-19 13:24:51 +0700

    Modify App.java

--- src/main/App.java
+++ src/main/App.java
@@ -15,9 +15,12 @@ public class App {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int sum = 0;
         for (int i = 0; i < 100; i++) {
-            System.out.println(i + " ");
+            sum += i;
         }
+        
+        System.out.println("Sum from 1 to 100 is " + sum);
     }
     
 }
